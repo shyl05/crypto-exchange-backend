81019991977f97315f081a22c8496ae752337322
Shylesh Thilagar <shylesh.thilagar@wisseninfotech.com>
2024-01-02 00:14:51 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+var indexRouter = require('./routes/index');
+var cryptoRouter = require('./routes/crypto');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+var corsOptions = {
+  origin: "*"
+};
+app.use(cors(corsOptions));
+
+app.use('/', indexRouter);
+app.use('/crypto', cryptoRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+// PORT
+const port = process.env.PORT || 7000
+
+app.listen(port,()=>{
+  console.log("Server is running on port : ",port)
+})
